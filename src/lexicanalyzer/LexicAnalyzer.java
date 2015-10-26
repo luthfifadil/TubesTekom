d510e61e0f01e89e7ae36d5a2022766c5a377eb8
luthfifadil <luthfifadilalamin@gmail.com>
2015-10-26 20:29:03 +0700

    Baru mulai

---
+++ src/lexicanalyzer/LexicAnalyzer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexicanalyzer;
+
+/**
+ *
+ * @author luthfifadil
+ */
+public class LexicAnalyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ganteng123 G = new Ganteng123("12E+5");
+        
+    }
+    
+}
